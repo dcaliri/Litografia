9eb00203ef9f5a99e33749dd4162dc014d81953b
jar <jimbokick@gmail.com>
2012-06-27 21:05:06 -0300

    primero

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Litografia::Application.config.secret_token = '8aa2aedfe12f336397ea62f0d05097b362ef144e1c3b95df7a71fcbe2c6777bc9957e7a6ec177e228951df24bf1b4b4db0f1b7c01526f4ef6c977629926bf8ff'
